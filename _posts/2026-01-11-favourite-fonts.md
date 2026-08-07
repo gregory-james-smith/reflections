9482b20d34c5bf98a5dbc1aaf62940988b44c4a2
Gregory Smith <13038259+gregory-james-smith@users.noreply.github.com>
2026-08-07 07:26:01 +0100

    Styling

--- _posts/2026-01-11-favourite-fonts.md
+++ _posts/2026-01-11-favourite-fonts.md
@@ -3,5 +3,5 @@ layout: post
 title: Favourite fonts
 ---
 
-- (Geist)[https://vercel.com/font]
-- (Cormorant)[https://www.behance.net/gallery/28579883/Cormorant-an-open-source-display-font-family]
+- [Geist](https://vercel.com/font)
+- [Cormorant](https://www.behance.net/gallery/28579883/Cormorant-an-open-source-display-font-family)
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,26 +1,60 @@
+// module.exports = {
+//   content: [
+//     './_includes/**/*.html',
+//     './_layouts/**/*.html',
+//     './_posts/**/*.md',
+//     './docs/**/*.md',
+//     './*.html'
+//   ],
+//   theme: {
+//     extend: {
+//       colors: {
+//         brand: {
+//           50: '#f3f8ff',
+//           100: '#dceeff',
+//           500: '#2f6fed',
+//           700: '#1e4ea8',
+//           900: '#142f5f'
+//         }
+//       },
+//       fontFamily: {
+//         sans: ['Inter', 'ui-sans-serif', 'system-ui', 'sans-serif']
+//       }
+//     }
+//   },
+//   plugins: []
+// };
+
 module.exports = {
+  darkMode: "class", // use light mode only
   content: [
-    './_includes/**/*.html',
-    './_layouts/**/*.html',
-    './_posts/**/*.md',
-    './docs/**/*.md',
-    './*.html'
+    "./_layouts/**/*.html",
+    "./_includes/**/*.html",
+    "./_posts/**/*.md",
+    "./assets/**/*.css",
+    "./*.html",
   ],
+  exclude: ["node_modules"],
   theme: {
     extend: {
-      colors: {
-        brand: {
-          50: '#f3f8ff',
-          100: '#dceeff',
-          500: '#2f6fed',
-          700: '#1e4ea8',
-          900: '#142f5f'
-        }
+      typography: {
+        DEFAULT: {
+          css: {
+            maxWidth: "65ch",
+            lineHeight: "1.55",
+            h1: { fontWeight: "600", letterSpacing: "-0.01em" },
+            h2: { fontWeight: "600", letterSpacing: "-0.01em" },
+            h3: { fontWeight: "500" },
+            code: { fontSize: "0.9em" },
+          },
+        },
       },
-      fontFamily: {
-        sans: ['Inter', 'ui-sans-serif', 'system-ui', 'sans-serif']
-      }
-    }
+      opacity: {
+        88: "0.88",
+        72: "0.72",
+        56: "0.56",
+      },
+    },
   },
-  plugins: []
+  plugins: [require("@tailwindcss/typography")],
 };
