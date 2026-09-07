0b7e3fdb30a5f722faf3efabcc3d757762f1a2d9
Gregory Smith <13038259+gregory-james-smith@users.noreply.github.com>
2026-09-07 20:52:00 +0100

    Custom ruby file to run Pandoc

--- Gemfile
+++ Gemfile
@@ -3,7 +3,6 @@ source "https://rubygems.org"
 gem "jekyll", "~> 4.3"
 gem "jekyll-feed", "~> 0.12"
 gem "jekyll-seo-tag", "~> 2.8"
-gem "jekyll-pandoc", "~> 2.0"
 
 platforms :mingw, :x64_mingw, :mswin, :jruby do
   gem "tzinfo", ">= 1", "< 3"
---
+++ _plugins/pandoc_markdown.rb
@@ -0,0 +1,32 @@
+module Jekyll
+  module Converters
+    class Markdown < Jekyll::Converter
+      safe true
+      priority :high
+
+      def matches(ext)
+        ext =~ /\.md\z/i
+      end
+
+      def output_ext(ext)
+        ".html"
+      end
+
+      def convert(content)
+        require "open3"
+
+        args = [
+          "pandoc",
+          "--from=markdown",
+          "--to=html5",
+          "--shift-heading-level-by=1"
+        ]
+
+        stdout, stderr, status = Open3.capture3(*args, stdin_data: content)
+        raise stderr unless status.success?
+
+        stdout
+      end
+    end
+  end
+end
