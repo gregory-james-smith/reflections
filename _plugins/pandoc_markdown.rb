8e9d6d8a812dc01a181aa957704d2ab9f68d7d7e
Gregory Smith <13038259+gregory-james-smith@users.noreply.github.com>
2026-09-19 22:45:44 +0100

    Math support for Markdown

--- _plugins/pandoc_markdown.rb
+++ _plugins/pandoc_markdown.rb
@@ -17,9 +17,10 @@ module Jekyll
 
         args = [
           "pandoc",
-          "--from=markdown+mark",
+          "--from=markdown+mark+tex_math_dollars",
           "--to=html5",
-          "--shift-heading-level-by=1"
+          "--shift-heading-level-by=1",
+          "--math-method=mathjax"
         ]
 
         stdout, stderr, status = Open3.capture3(*args, stdin_data: content)
